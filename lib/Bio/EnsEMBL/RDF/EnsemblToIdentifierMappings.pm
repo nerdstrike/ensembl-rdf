2d5537c2e2e92d46d76e6f2f4c66065f412d0865
Kieron Taylor <ktaylor@ebi.ac.uk>
2015-09-17 16:34:09 +0900

    Put Linked Data mappings into JSON and use as a basis for generating SPARQL and RDF from Ensembl databases. New script to show querying of Pubmed via SPARQL. Needs more work

---
+++ lib/Bio/EnsEMBL/RDF/EnsemblToIdentifierMappings.pm
@@ -0,0 +1,66 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 NAME
+
+    EnsemblToIdentifierMappings - Module to help map Ensembl Xrefs to Identifiers.org namespaces
+
+=head1 SYNOPSIS
+
+  
+=head1 DESCRIPTION
+
+
+=cut
+
+package Bio::EnsEMBL::RDF::EnsemblToIdentifierMappings;
+
+use strict;
+use JSON qw/decode_json/;
+use IO::File;
+sub new {
+  my ($class,$xref_mapping_file) = @_;
+  local $/;
+  my $fh = IO::File->new($xref_mapping_file,'r');
+  my $json = <$fh>;
+  my $doc = decode_json($json);
+  my %xref_mapping;
+  map { $xref_mapping{$_->{db_name}} = $_ } @{ $doc->{mappings} };
+  bless({ xref_mapping => \%xref_mapping },$class);
+}
+# For a given Ensembl ExternalDB name, gives a hash containing any of:
+# db_name - Ensembl internal name for an external DB
+# example_id
+# "standard abbreviation" id_namespace
+# canoncal_LOD - or base URI LOD = Linked Open Data
+# URI_type - the class of things a URI belongs to for this source
+# ignore (boolean)
+# EDAM_type
+# EDAM_term
+# regex - used to transform a textual ID into a URI
+sub get_mapping {
+  my $self = shift;
+  my $short_name = shift;
+  my $mappings = $self->{xref_mapping};
+  if (exists $mappings->{$short_name}) {
+    return $mappings->{$short_name};
+  } else {
+    return;
+  }
+}
+
+
+1;
--- scripts/sparqling_pubmed.pl
+++ scripts/sparqling_pubmed.pl
@@ -6,6 +6,8 @@ use JSON;
 use URI::Escape;
 use RDF::Query::Client;
 use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::RDF::RDFlib;
+use Bio::EnsEMBL::RDF::EnsemblToIdentifierMappings;
 use Time::HiRes qw/gettimeofday tv_interval/;
 
 my $uri = 'http://wwwdev.ebi.ac.uk/rdf/services/textmining/sparql';
@@ -16,52 +18,70 @@ my $lwp = LWP::UserAgent->new();
 $lwp->agent('ensembl-xref  (Linux, perl/5.14.2)');
 
 Bio::EnsEMBL::Registry->load_registry_from_db(
-  -host => 'mysql-ensembl-mirror.ebi.ac.uk',
-  -user => 'anonymous',
-  -port => 4240,
-  # -host => 'asiadb.ensembl.org',
+  # -host => 'mysql-ensembl-mirror.ebi.ac.uk',
   # -user => 'anonymous',
-  # -port => 5306,
+  # -port => 4240,
+  -host => 'asiadb.ensembl.org',
+  -user => 'anonymous',
+  -port => 5306,
   -db_version => 81,
   );
 print "Connected to Ensembl";
 
-my $sparql_start = 'PREFIX dcterms: <http://purl.org/dc/terms/>
-PREFIX oa: <http://www.w3.org/ns/oa#>
+my $converter = Bio::EnsEMBL::RDF::EnsemblToIdentifierMappings->new('../xref_LOD_mapping.json');
 
+my $sparql_prefix = compatible_name_spaces();
+
+my $sparql_start = '
 SELECT ?prefix ?exact ?postfix ?section ?source WHERE {
-?annotation oa:hasBody <http://identifiers.org/ensembl/';
-
-my $sparql_end = '>.
-?annotation oa:hasTarget ?target.
-?target oa:hasSource ?source.
-?target oa:hasSelector ?selector.
-?target dcterms:isPartOf ?section.
-?selector oa:prefix ?prefix.
-?selector oa:exact ?exact.
-?selector oa:postfix ?postfix.
-}';
-
+  ?annotation oa:hasBody ?xref .
+  ?annotation oa:hasTarget ?target .
+  ?target oa:hasSource ?source .
+  ?target oa:hasSelector ?selector .
+  ?target dcterms:isPartOf ?section .
+  ?selector oa:prefix ?prefix .
+  ?selector oa:exact ?exact .
+  ?selector oa:postfix ?postfix .
+';
+my $sparql_end = '}';
 my $gene_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Human','core','Gene');
 my $genes =  $gene_adaptor->fetch_all();
 my $sparql;
 
 my $hits = 0;
+my @lits;
 print "Querying Pubmed beta\n"; 
+  use Data::Dumper;
 
 my $start_time = [gettimeofday()];
 while ( my $gene = shift @$genes) {
-  $sparql = $sparql_start . $gene->stable_id . $sparql_end;
-  my $query = RDF::Query::Client->new($sparql, {UserAgent => $lwp});
-  my @results = $query->execute($uri);
-  use Data::Dumper;
-  while (my $row = shift @results) {
-    print Dumper $row;
-    print $row->{source}->as_string."\n";
+  my @xrefs = @{ $gene->get_all_DBLinks };
+  my @xref_names;
+  foreach my $xref (@xrefs) {
+    my $mapping = $converter->get_mapping($xref->dbname);
+    if (exists $mapping->{canonical_LOD} && ! exists $mapping->{ignore}) {
+      push @xref_names,$mapping->{canonical_LOD}.$xref->display_id;
+    } else {
+      print "No mapping found for ".$xref->dbname."\n";
+    }
   }
-  $hits++;
+  if (@xref_names > 0) {
+    my $sparql_values = sprintf 'VALUES ?xref {%s}'.join ' ', map { qq{$_}} @xref_names;
+    $sparql = $sparql_prefix . $sparql_start . $sparql_values . $sparql_end;
+    my $query = RDF::Query::Client->new($sparql, {UserAgent => $lwp});
+    my @results = $query->execute($uri);
+    while (my $row = shift @results) {
+      print $row->{source}->as_string."\n";
+      push @lits,$row->{source}->as_string;
+    }
+    $hits++;
+  }
+  
+  last if $hits == 50;
 }
 my $elapsed = tv_interval ( $start_time, [gettimeofday()]);
-printf "Total of %s Ensembl genes found in Pubmed beta\n",$hits;
+print join "\n",@lits;
+printf "\nTotal of %s Ensembl genes found in Pubmed beta\n",$hits;
 printf "Total time is %.2f seconds\n",$elapsed;
+printf "Matched %d PubMed articles\n",@lits;
 print "Finished\n";
