b869f3e404e76c8782e93066fa6f19ccb7f03f7d
Kieron Taylor <ktaylor@ebi.ac.uk>
2015-10-09 11:15:19 +0100

    Now with added success.

--- lib/Bio/EnsEMBL/RDF/Pipeline/Process/RDFDump.pm
+++ lib/Bio/EnsEMBL/RDF/Pipeline/Process/RDFDump.pm
@@ -29,6 +29,7 @@ use strict;
 use parent ('Bio::EnsEMBL::RDF::Pipeline::Base');
 use Bio::EnsEMBL::RDF::EnsemblToTripleConverter;
 use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Utils::IO qw/work_with_file/;
 
 sub fetch_input {
     my $self = shift;
@@ -42,25 +43,26 @@ sub run {
     my $self = shift;
     my $species = $self->param('species');
     my $config_file = $self->param('config_file'); # config required for mapping Ensembl things to RDF (xref_LOD_mapping.json)
-    my $path = $self->get_dir("$species.rdf");
-    
-    my $dba = $self->get_DBAdaptor;
-    
-    my $ontology_adaptor = Bio::EnsEMBL::Registry->get_adaptor('multi','ontology','OntologyTerm');
-    my $meta_adaptor = $dba->get_MetaContainer;
-    my $release = $self->param('release');
+    my $path = $self->get_dir();
+    $path .= '/'.$species.".rdf";
+    work_with_file( $path, 'w', sub {
+        my $dba = $self->get_DBAdaptor;
+        
+        my $ontology_adaptor = Bio::EnsEMBL::Registry->get_adaptor('multi','ontology','OntologyTerm');
+        my $meta_adaptor = $dba->get_MetaContainer;
+        my $release = $self->param('release');
 
-    my $triple_converter = Bio::EnsEMBL::RDF::EnsemblToTripleConverter->new($ontology_adaptor, $meta_adaptor, $species, undef, $release, $config_file);
-    $triple_converter->write_to_file($path);
+        my $triple_converter = Bio::EnsEMBL::RDF::EnsemblToTripleConverter->new($ontology_adaptor, $meta_adaptor, $species, undef, $release, $config_file);
+        $triple_converter->write_to_file($path);
 
-    $triple_converter->print_namespaces;
-    $triple_converter->print_species_info;
+        $triple_converter->print_namespaces;
+        $triple_converter->print_species_info;
 
-
-    my $is_human;
-    $is_human = 1 if $species eq 'homo_sapiens';
-    my $slices = $self->get_Slices(undef,$is_human); # see Production::Pipeline::Base;
-    $triple_converter->print_seq_regions($slices);
+        my $is_human;
+        $is_human = 1 if $species eq 'homo_sapiens';
+        my $slices = $self->get_Slices(undef,$is_human); # see Production::Pipeline::Base;
+        $triple_converter->print_seq_regions($slices);
+    });
 }
 
 
