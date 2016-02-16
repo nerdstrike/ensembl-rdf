f69094bdf3adcf9c86bb7e43689db87662d1d267
Kieron Taylor <ktaylor@ebi.ac.uk>
2016-02-16 17:40:04 +0000

    Apparently commas were necessary.

--- lodestar-ui/src/main/webapp/scripts/namespaces.js
+++ lodestar-ui/src/main/webapp/scripts/namespaces.js
@@ -11,20 +11,20 @@
  */
 
 var lodeNamespacePrefixes = {
-    rdf: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#>'
-    rdfs: '<http://www.w3.org/2000/01/rdf-schema#>'
-    dcterms: '<http://purl.org/dc/terms/>'
-    dc: '<http://purl.org/dc/elements/1.1/>'
-    obo: '<http://purl.obolibrary.org/obo/>'
-    sio: '<http://semanticscience.org/resource/>'
-    faldo: '<http://biohackathon.org/resource/faldo#>'
-    ensembl: '<http://rdf.ebi.ac.uk/resource/ensembl/>'
-    ensembltranscript: '<http://rdf.ebi.ac.uk/resource/ensembl.transcript/>'
-    ensemblexon: '<http://rdf.ebi.ac.uk/resource/ensembl.exon/>'
-    ensemblprotein: '<http://rdf.ebi.ac.uk/resource/ensembl.protein/>'
-    ensemblterms: '<http://rdf.ebi.ac.uk/terms/ensembl/>'
-    identifiers: '<http://identifiers.org/>'
-    skos: '<http://www.w3.org/2004/02/skos/core#>'
+    rdf: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#>',
+    rdfs: '<http://www.w3.org/2000/01/rdf-schema#>',
+    dcterms: '<http://purl.org/dc/terms/>',
+    dc: '<http://purl.org/dc/elements/1.1/>',
+    obo: '<http://purl.obolibrary.org/obo/>',
+    sio: '<http://semanticscience.org/resource/>',
+    faldo: '<http://biohackathon.org/resource/faldo#>',
+    ensembl: '<http://rdf.ebi.ac.uk/resource/ensembl/>',
+    ensembltranscript: '<http://rdf.ebi.ac.uk/resource/ensembl.transcript/>',
+    ensemblexon: '<http://rdf.ebi.ac.uk/resource/ensembl.exon/>',
+    ensemblprotein: '<http://rdf.ebi.ac.uk/resource/ensembl.protein/>',
+    ensemblterms: '<http://rdf.ebi.ac.uk/terms/ensembl/>',
+    identifiers: '<http://identifiers.org/>',
+    skos: '<http://www.w3.org/2004/02/skos/core#>',
     core: '<http://purl.uniprot.org/core/>'
 };
 
